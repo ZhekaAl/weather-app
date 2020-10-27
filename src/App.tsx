c897c3e8a6808ab1e9708599ec3990bb5560dc15
AladovEvgenii <aladof@ya.ru>
2020-10-27 22:09:43 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CityChoice from './city-choice';
+import CityWeather from './city-weather';
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import 'leda/dist/styles/leda.light.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CityChoice />
+      <CityWeather />
     </div>
   );
 }
