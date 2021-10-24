d906cef1a1dcd66434e44ef57fb58c53f657b7d2
Evgeny Al <aladof@ya.ru>
2021-10-24 22:48:37 +0300

    mf city param

--- src/@types/app2.d.ts
+++ src/@types/app2.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="react" />
 
+// import { City } from "@src/store/types";
+// https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts
+type City = import('@src/store/types').City;
+
 declare module "app2/Button" {
   const Button: React.ComponentType;
 
@@ -7,7 +11,7 @@ declare module "app2/Button" {
 }
 
 declare module "app2/App" {
-  const App: React.ComponentType;
+  const App: React.ComponentType<{city?: City} >;
 
   export default App;
 }
--- src/mf-pages/graphics-mf.tsx
+++ src/mf-pages/graphics-mf.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 
+import { useCurrentWeather } from '../../src/hooks/use-current-weather';
+
 const RemoteApp = React.lazy(() => import('app2/App'));
 
 const GraphicsMF: FC = () => {
+  const { city } = useCurrentWeather();
   return (
     <React.Suspense fallback="Загрузка страницы Графики">
-      <RemoteApp />
+      <RemoteApp city={city} />
     </React.Suspense>
   );
 };
